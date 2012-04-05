f3b53833a44fcf470ca66ca17fdf646e8dea1b4c
Henrik <heinrisch@gmail.com>
2012-04-05 15:59:44 +0200

    Saving access token

--- src/heinrisch/friendlist/view/Main.java
+++ src/heinrisch/friendlist/view/Main.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 
 import com.facebook.android.DialogError;
@@ -17,30 +19,59 @@ import com.facebook.android.FacebookError;
 public class Main extends Activity {
    
     Facebook facebook = new Facebook("424405194241987");
+    
+    String TOKENSAVE = "LoginToken";
+    
+    SharedPreferences sharedPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-
-       
-        Button login = (Button) findViewById(R.id.login_button);
         
+        //Check if we already have an access token
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        String access_token = sharedPreferences.getString("access_token", null);
+        long expires = sharedPreferences.getLong("access_expires", 0);
+        
+        if(access_token != null) {
+            facebook.setAccessToken(access_token);
+        }
+        if(expires != 0) {
+            facebook.setAccessExpires(expires);
+        }
+        
+        
+        //Session is valid we are good to go, else we stay at the login screen
+        if(facebook.isSessionValid()){
+        	
+        	
+        }
+        
+        Button login = (Button) findViewById(R.id.login_button);
         login.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				autorizeWithFacebook();
+				authorizeWithFacebook();
 				
 			}
 		});
         
     }
 
-    protected void autorizeWithFacebook() {
+    protected void authorizeWithFacebook() {
     	facebook.authorize(this, new DialogListener() {
             @Override
-            public void onComplete(Bundle values) {}
+            public void onComplete(Bundle values) {
+            	//Save access token on successful login
+            	SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("access_token", facebook.getAccessToken());
+                editor.putLong("access_expires", facebook.getAccessExpires());
+                editor.commit();
+            	
+            }
 
             @Override
             public void onFacebookError(FacebookError error) {
