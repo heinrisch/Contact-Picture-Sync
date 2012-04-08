2fa9cc5270d7dc92c7153685ff9fadce6e78e7c0
Henrik <heinrisch@gmail.com>
2012-04-08 15:59:53 +0200

    adding contacthandler for handeling local contacts

--- src/heinrisch/contact/picture/sync/Main.java
+++ src/heinrisch/contact/picture/sync/Main.java
@@ -27,6 +27,9 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
+		
+		ContactHandler ch = new ContactHandler(this);
+		ch.readContacts();
 
 		//Check if we already have an access token
 		sharedPreferences = getPreferences(MODE_PRIVATE);
